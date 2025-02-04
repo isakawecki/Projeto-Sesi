78cf18b9db13a02ee037d215242d4ab0e6953b61
isakawecki <isakawecki@gmail.com>
2025-02-04 13:27:33 -0300

    Delete projeto directory

--- projeto/src/main/java/com/sesi/projeto/dto/ProdutoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.sesi.projeto.dto;
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-public record ProdutoDTO(String nome, double preco, String descricao) {
-
-}
